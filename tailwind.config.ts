fed964fdae812b3bb5e9c38dc0fd57b0315f77dc
Facundo Remmer <facundoremmer0@gmail.com>
2025-12-10 12:45:23 -0300

    Fix rutas case sensitive tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,15 +3,19 @@ import type { Config } from 'tailwindcss';
 const config: Config = {
   darkMode: 'class',
   content: [
-    // Cubrimos si usás carpeta 'src'
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    
-    // Cubrimos si NO usás carpeta 'src' (tu config actual)
+    // Opción 1: Estándar (todo minúscula)
     './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+
+    // Opción 2: Posibles Mayúsculas (El error común en Windows)
+    './App/**/*.{js,ts,jsx,tsx,mdx}',
+    './Components/**/*.{js,ts,jsx,tsx,mdx}',
+    './Pages/**/*.{js,ts,jsx,tsx,mdx}',
+
+    // Opción 3: Por si usas carpeta 'src'
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {},
