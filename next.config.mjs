d5ed9c16887d23fc54e8735fd2e942d88ac642f8
Facundo Remmer <facundoremmer0@gmail.com>
2025-12-10 14:25:31 -0300

    Configuracion de next

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-const repo = 'portfolio'; // nombre del repo en GitHub
 
-export default {
-  output: 'export',
+const nextConfig = {
+  output: 'export', 
+  
   images: { unoptimized: true },
-  basePath: isProd ? `/${repo}` : '',
-  assetPrefix: isProd ? `/${repo}/` : ''
+
 };
+
+export default nextConfig;
